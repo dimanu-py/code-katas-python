c1ff7b79ad6193c2de18de163988992f11f847bf
dimanu-py <diegomtz126@gmail.com>
2023-12-30 00:29:43 +0100

    Refactor leap year function and create a class
    After extracting the methods to make more explicit the checking if a year is divisible or not by a number, it seemed correct to create a class and encapsulate the logic. This also means refactoring how test are written

--- leap-year/leap_year.py
+++ leap-year/leap_year.py
@@ -1,18 +1,20 @@
 
 
-def is_leap_year(year: int) -> bool:
-    """
-    Returns True if a year is leap, False otherwise
+class Year:
+    def __init__(self, year: int) -> None:
+        self.year = year
 
-    A year is leap if is divisible by 4, and if is divisible by 100 and by 400.
-    This means that a year is leap if is divisible by 4 and not by 100, or if is divisible by 400.
-    """
-    return year_divisible_by(4, year) and (year_not_divisible_by(100, year) or year_divisible_by(400, year))
+    def is_leap(self) -> bool:
+        """
+        Returns True if a year is leap, False otherwise
 
+        A year is leap if is divisible by 4, and if is divisible by 100 and by 400.
+        This means that a year is leap if is divisible by 4 and not by 100, or if is divisible by 400.
+        """
+        return self.divisible_by(4) and (self.not_divisible_by(100) or self.divisible_by(400))
 
-def year_divisible_by(number: int, year: int) -> bool:
-    return year % number == 0
+    def divisible_by(self, number: int) -> bool:
+        return self.year % number == 0
 
-
-def year_not_divisible_by(number: int, year: int) -> bool:
-    return year % number != 0
+    def not_divisible_by(self, number: int) -> bool:
+        return self.year % number != 0
--- leap-year/test_leap_year.py
+++ leap-year/test_leap_year.py
@@ -1,25 +1,29 @@
 import pytest
-from leap_year import is_leap_year
+from leap_year import Year
 
 
-@pytest.mark.parametrize("year", [1993, 1997, 2001, 2005, 2009])
-def test_not_leap_year(year: int):
+@pytest.fixture()
+def year(year_number: int) -> Year:
+    return Year(year_number)
+
+@pytest.mark.parametrize("year_number", [1993, 1997, 2001, 2005, 2009])
+def test_not_leap_year(year: Year, year_number: int):
     """Asserts that a year is not leap because is not divisible by 4"""
-    assert is_leap_year(year) == False
+    assert year.is_leap() == False
 
-@pytest.mark.parametrize("year", [1996, 2004, 2008, 2012, 2016])
-def test_leap_year_divisible_by_4(year: int):
+@pytest.mark.parametrize("year_number", [1996, 2004, 2008, 2012, 2016])
+def test_leap_year_divisible_by_4(year: Year, year_number: int):
     """Asserts that a year is leap when is divisible by 4"""
-    assert is_leap_year(year) == True
+    assert year.is_leap() == True
 
 
-@pytest.mark.parametrize("year", [1700, 1800, 1900, 2100, 2200])
-def test_not_leap_year_divisible_by_100(year: int):
+@pytest.mark.parametrize("year_number", [1700, 1800, 1900, 2100, 2200])
+def test_not_leap_year_divisible_by_100(year: Year, year_number: int):
     """Asserts that a year is not leap when is divisible by 100 but not by 400"""
-    assert is_leap_year(year) == False
+    assert year.is_leap() == False
 
 
-@pytest.mark.parametrize("year", [1600, 2000, 2400, 2800, 3200])
-def test_leap_year_divisible_by_100_and_400(year: int):
+@pytest.mark.parametrize("year_number", [1600, 2000, 2400, 2800, 3200])
+def test_leap_year_divisible_by_100_and_400(year: Year, year_number: int):
     """Asserts that a year is leap when is divisible by 4 and by 400"""
-    assert is_leap_year(year) == True
+    assert year.is_leap() == True
