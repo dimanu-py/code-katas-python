b932b2ec95241b9d9c44d3c5064cf35bde4b9e57
dimanu-py <diegomtz126@gmail.com>
2023-12-29 23:50:42 +0100

    Add test to verify leap year when divisible by 100 and 400
    When the year is divisible by 100 and by 400, it should be returning True

--- leap-year/test_leap_year.py
+++ leap-year/test_leap_year.py
@@ -1,6 +1,7 @@
 import pytest
 from leap_year import is_leap_year
 
+
 @pytest.mark.parametrize("year", [1993, 1997, 2001, 2005, 2009])
 def test_not_leap_year(year: int):
     """Asserts that a year is not leap because is not divisible by 4"""
@@ -14,5 +15,11 @@ def test_leap_year_divisible_by_4(year: int):
 
 @pytest.mark.parametrize("year", [1700, 1800, 1900, 2100, 2200])
 def test_not_leap_year_divisible_by_100(year: int):
-    """Asserts that a year is leap when is divisible by 4 and by 400"""
+    """Asserts that a year is not leap when is divisible by 100 but not by 400"""
     assert is_leap_year(year) == False
+
+
+@pytest.mark.parametrize("year", [1600, 2000, 2400, 2800, 3200])
+def test_leap_year_divisible_by_100_and_400(year: int):
+    """Asserts that a year is leap when is divisible by 4 and by 400"""
+    assert is_leap_year(year) == True
