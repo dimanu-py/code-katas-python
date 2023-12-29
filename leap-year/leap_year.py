684cb5167f51a35e620356920706f7af36ed18ad
dimanu-py <diegomtz126@gmail.com>
2023-12-29 23:35:43 +0100

    Implement logic to detect a year is leap when is divisible by 100 and 400
    When a year is divisible by 100 but not by 400, then is not leap.

--- leap-year/leap_year.py
+++ leap-year/leap_year.py
@@ -1,4 +1,11 @@
 
 
 def is_leap_year(year: int) -> bool:
-    return year % 4 == 0
+
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            return False
+        return True
+    return False
