1d99584823202eab470a8be942aded7c23465d33
dimanu-py <diegomtz126@gmail.com>
2024-01-25 12:20:25 +0100

    feat: function should return the same word for words with only one syllable

--- anagrams/anagrams.py
+++ anagrams/anagrams.py
@@ -4,4 +4,4 @@ def anagrams(word: str) -> list[str]:
     """
     Return a list of all the possible anagrams of a word.
     """
-    return [""]
+    return [word]
--- anagrams/test_anagrams.py
+++ anagrams/test_anagrams.py
@@ -1,7 +1,12 @@
 from anagrams.anagrams import anagrams
+import pytest
 
 
 class TestAnagramsShould:
 
-    def test_return_empty_list_when_no_word_is_given(self):
+    def test_return_empty_list_when_no_word_is_given(self) -> None:
         assert anagrams("") == [""]
+
+    @pytest.mark.parametrize("word", ["a", "b", "c"])
+    def test_return_same_word_when_only_has_one_syllable(self, word: str) -> None:
+        assert anagrams(word) == [word]
