889170a9ec3b2a5cb3307aeff9022d454ff60f94
dimanu-py <diegomtz126@gmail.com>
2024-02-12 09:46:44 +0100

    Add test case for edge cases

--- roman-numerals/test_roman_numerals.py
+++ roman-numerals/test_roman_numerals.py
@@ -10,7 +10,8 @@ class TestRomanNumeralShould:
          (10, "X"), (11, "XI"), (12, "XII"), (13, "XIII"), (14, "XIV"), (15, "XV"), (16, "XVI"), (17, "XVII"), (18, "XVIII"),
          (19, "XIX"), (20, "XX"), (21, "XXI"), (22, "XXII"), (23, "XXIII"), (24, "XXIV"), (25, "XXV"), (26, "XXVI"), (27, "XXVII"),
          (30, "XXX"), (40, "XL"), (50, "L"), (60, "LX"), (70, "LXX"), (80, "LXXX"), (90, "XC"), (100, "C"), (200, "CC"), (300, "CCC"),
-         (400, "CD"), (500, "D"), (600, "DC"), (700, "DCC"), (800, "DCCC"), (900, "CM"), (1000, "M")]
+         (400, "CD"), (500, "D"), (600, "DC"), (700, "DCC"), (800, "DCCC"), (900, "CM"), (1000, "M"), (846, "DCCCXLVI"),
+         (1999, "MCMXCIX"), (2008, "MMVIII"), (3999, "MMMCMXCIX")]
     )
     def test_convert_number_to_numeral(self, number: int, expected: str) -> None:
 
