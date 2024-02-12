fc7c9f507e7e989897079b891ae55fd25877cfd7
dimanu-py <diegomtz126@gmail.com>
2024-02-12 09:21:23 +0100

    Add tests fpr 15, 16 and 17 use cases

--- roman-numerals/test_roman_numerals.py
+++ roman-numerals/test_roman_numerals.py
@@ -7,7 +7,9 @@ class TestRomanNumeralShould:
     @pytest.mark.parametrize(
         "number, expected",
         [(1, "I"), (2, "II"), (3, "III"), (4, "IV"), (5, "V"), (6, "VI"), (7, "VII"), (8, "VIII"), (9, "IX"),
-         (10, "X"), (11, "XI"), (12, "XII"), (13, "XIII"), (14, "XIV")]
+         (10, "X"), (11, "XI"), (12, "XII"), (13, "XIII"), (14, "XIV"), (15, "XV"), (16, "XVI"), (17, "XVII"), (18, "XVIII"),
+         (19, "XIX"), (20, "XX"), (21, "XXI"), (22, "XXII"), (23, "XXIII"), (24, "XXIV"), (25, "XXV"), (26, "XXVI"), (27, "XXVII"),
+         (30, "XXX")]
     )
     def test_convert_number_to_numeral(self, number: int, expected: str) -> None:
 
