e5645123ee5813ca5f84b07fdf641983e483c8d3
dimanu-py <diegomtz126@gmail.com>
2024-02-16 19:27:15 +0100

    refactor: move normal items tests to single class to identify behaviour easily

--- gilded-rose/test_gilded_rose.py
+++ gilded-rose/test_gilded_rose.py
@@ -1,11 +1,14 @@
 from gilded_rose import Item, GildedRose
 
 
-class TestGildedRose:
+class TestNormalItem:
+
     def test_item_name_does_not_change(self) -> None:
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
+
         gilded_rose.update_quality()
+
         assert "foo" == items[0].name
 
     def test_item_quality_decreases(self) -> None:
@@ -39,6 +42,9 @@ class TestGildedRose:
         gilded_rose.update_quality()
 
         assert items[0].quality >= 0
+
+
+class TestGildedRose:
 
     def test_aged_brie_increases_quality_with_age(self) -> None:
         items = [Item("Aged Brie", 1, 0)]
