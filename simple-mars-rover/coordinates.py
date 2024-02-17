817e79454236df9fa8e9bed5a15261aff54476d0
dimanu-py <diegomtz126@gmail.com>
2024-02-17 12:32:02 +0100

    feat: add logic to wrap around the rover's movement in the grid

--- simple-mars-rover/coordinates.py
+++ simple-mars-rover/coordinates.py
@@ -5,3 +5,15 @@ class Coordinates:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
+
+        if self.x < 0:
+            self.x = 9
+
+        if self.y < 0:
+            self.y = 9
+
+        if self.x > 9:
+            self.x = 0
+
+        if self.y > 9:
+            self.y = 0
