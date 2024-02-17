0ffe39cb6e97376131412487a12bbb13a3e03656
dimanu-py <diegomtz126@gmail.com>
2024-02-17 12:31:17 +0100

    fix: update test structure to new mars rover structure

--- simple-mars-rover/test_mars_rover.py
+++ simple-mars-rover/test_mars_rover.py
@@ -11,7 +11,7 @@ class TestMarsRover:
 
     def test_initial_position_is_0_0_N(self, rover: MarsRover):
 
-        position = f"{rover.x_coordinate}:{rover.y_coordinate}:{rover.orientation}"
+        position = f"{rover.coordinates.x}:{rover.coordinates.y}:{rover.orientation}"
         assert position == "0:0:N"
 
     @pytest.mark.parametrize(
