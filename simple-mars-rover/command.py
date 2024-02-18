2534066869e9cf0578d99690f3809d7b540c8557
dimanu-py <diegomtz126@gmail.com>
2024-02-18 01:02:24 +0100

    feat: add specific command to move forward

--- simple-mars-rover/command.py
+++ simple-mars-rover/command.py
@@ -1,8 +1,20 @@
 from abc import ABC, abstractmethod
 
+from mars_rover import MarsRover
+
 
 class Command(ABC):
 
     @abstractmethod
     def execute(self) -> None:
         """Execute the command."""
+
+
+class MoveForward(Command):
+    """Move the rover one grid point in the current direction."""
+
+    def __init__(self, rover: MarsRover) -> None:
+        self.rover = rover
+
+    def execute(self) -> None:
+        self.rover.move_forward()
