a29d01c95ab32c684eeecb8678aee945b0fc6d43
dimanu-py <diegomtz126@gmail.com>
2024-02-23 17:58:55 +0100

    feat: implement logic to get anagrams of two letters
    This algorithm is composed of two for loop: the first one will get one by one the letters of the word, fixing that letter into the first position of the combination. The second loop will call again the function with the rest of the word, repeating the same operation until all the letters are done for the fixed letter. This operation will be then repeated to the rest of the letters

--- anagrams/anagrams.py
+++ anagrams/anagrams.py
@@ -4,4 +4,12 @@ def anagrams(word: str) -> list[str]:
     """
     Return a list of all the possible anagrams of a word.
     """
-    return [word]
+    if len(word) <= 1:
+        return [word]
+
+    anagrams_results = []
+    for index, fixed_letter in enumerate(word):
+        for anagram in anagrams(word[:index] + word[index + 1:]):
+            anagrams_results.append(fixed_letter + anagram)
+
+    return anagrams_results
--- anagrams/test_anagrams.py
+++ anagrams/test_anagrams.py
@@ -10,3 +10,6 @@ class TestAnagramsShould:
     @pytest.mark.parametrize("word", ["a", "b", "c"])
     def test_return_same_word_when_only_has_one_syllable(self, word: str) -> None:
         assert anagrams(word) == [word]
+
+    def test_return_anagrams_when_word_has_two_syllables(self) -> None:
+        assert anagrams("ab") == ["ab", "ba"]
