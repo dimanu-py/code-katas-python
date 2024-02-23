db86b07bac3a8bfba0844219d5eb1c015417fe88
dimanu-py <diegomtz126@gmail.com>
2024-02-23 18:09:11 +0100

    test: add test case for abc and biro (kata example)

--- anagrams/test_anagrams.py
+++ anagrams/test_anagrams.py
@@ -7,9 +7,14 @@ class TestAnagramsShould:
     def test_return_empty_list_when_no_word_is_given(self) -> None:
         assert anagrams("") == [""]
 
-    @pytest.mark.parametrize("word", ["a", "b", "c"])
-    def test_return_same_word_when_only_has_one_syllable(self, word: str) -> None:
-        assert anagrams(word) == [word]
-
-    def test_return_anagrams_when_word_has_two_syllables(self) -> None:
-        assert anagrams("ab") == ["ab", "ba"]
+    @pytest.mark.parametrize(
+        "word, expected",
+        [
+            ("a", ["a"]),
+            ("b", ["b"]),
+            ("ab", ["ab", "ba"]),
+            ("abc", ["abc", "acb", "bac", "bca", "cab", "cba"]),
+            ("biro", ["biro", "bior", "brio", "broi", "boir", "bori", "ibro", "ibor", "irbo", "irob", "iobr", "iorb", "rbio", "rboi", "ribo", "riob", "robi", "roib", "obir", "obri", "oibr", "oirb", "orbi", "orib"])]
+    )
+    def test_return_all_anagrams(self, word: str, expected: list[str]) -> None:
+        assert anagrams(word) == expected
